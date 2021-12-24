8e8219de92e555d3c8f37b05f963b388e070a264
lefmarna <panume5ya@gmail.com>
2021-12-24 18:14:10 +0900

    init

---
+++ pages/inspire/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const text = 'Hello, World!'
+
+    return {
+      text,
+    }
+  },
+  head: {
+    title: 'my page222',
+    meta: [
+      {
+        name: 'description',
+        content: '説明です',
+      },
+      {
+        name: 'twitter:description',
+        content: '説明ですね',
+      },
+      {
+        property: 'og:description',
+        content: '説明ですね',
+      },
+    ],
+  },
+})
+</script>
