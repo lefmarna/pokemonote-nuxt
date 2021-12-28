bf3f784af2452e033f82672c84435546252a6a1f
lefmarna <panume5ya@gmail.com>
2021-12-28 23:55:58 +0900

    nuxtの基本設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - pokemonote-web',
-    title: 'pokemonote-web',
+    titleTemplate: '%s | Pokemonote',
+    title: 'Pokemonote',
     htmlAttrs: {
       lang: 'ja',
     },
@@ -57,9 +57,9 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
-        dark: {
+        light: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
