83a790c8e40e31500d13e552cef31145bd27556e
lefmarna <panume5ya@gmail.com>
2022-01-03 19:19:55 +0900

    ESLintの修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,7 @@ module.exports = {
   plugins: [],
   // add your custom rules here
   rules: {
-    'vue/multi-word-component-names': [
-      'error',
-      {
-        ignores: ['index'],
-      },
-    ],
+    'no-console': 'off',
+    'vue/multi-word-component-names': 'off',
   },
 }
