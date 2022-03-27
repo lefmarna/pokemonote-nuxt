ac30b3993cd6ff5c2183cf200aabd8e3d4560431
lefmarna <panume5ya@gmail.com>
2022-03-27 11:55:17 +0900

    feat: エラーページを用意

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,13 +1,19 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
-  </v-app>
+  <v-container>
+    <v-card flat>
+      <!-- タイトル -->
+      <v-card-title v-if="error.statusCode === 404">
+        {{ pageNotFound }}
+      </v-card-title>
+      <v-card-title v-else>
+        {{ otherError }}
+      </v-card-title>
+      <v-card-text>お探しのページは見つかりませんでした。</v-card-text>
+      <v-card-actions>
+        <v-btn text color="primary" to="/" plain nuxt>トップページへ戻る</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
