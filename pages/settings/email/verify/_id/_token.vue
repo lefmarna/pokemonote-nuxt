51fd20c9b751b920ffdc064e5168931eec460ea1
lefmarna <panume5ya@gmail.com>
2023-02-04 10:13:58 +0900

    feat: メールアドレス更新時のフロント実装を完了

--- pages/settings/email/verify/_id/_token.vue
+++ pages/settings/email/verify/_id/_token.vue
@@ -1,18 +1,24 @@
 <template>
-  <div>メールアドレスの変更を確認中...</div>
+  <div v-if="isConfirm">メールアドレスの変更を確認中...</div>
+  <v-container v-else>
+    <Title text="メールアドレス更新完了" />
+    <p>メールアドレスの更新が完了しました。</p>
+    <v-btn to="/" nuxt>トップページへ戻る</v-btn>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { defineComponent, useContext, useRouter } from '@nuxtjs/composition-api'
+import { defineComponent, ref, useContext, useRouter } from '@nuxtjs/composition-api'
 import { updateMeta } from '~/utils/utilities'
 
 export default defineComponent({
   middleware: 'auth',
   setup() {
-    updateMeta('メールアドレスの変更を確認')
+    updateMeta('メールアドレスの更新')
 
     const { $axios, route, store } = useContext()
     const router = useRouter()
+    const isConfirm = ref(true)
 
     ;(async () => {
       try {
@@ -20,10 +26,15 @@ export default defineComponent({
           `/settings/email/verify/${route.value.params.id}/${route.value.params.token}/?expires=${route.value.query.expires}&signature=${route.value.query.signature}`
         )
         store.commit('updateAuthUser', response.data.data)
+        isConfirm.value = false
       } catch (error) {
         console.log(error)
+        router.push('/')
       }
-      router.push('/')
+
+      return {
+        isConfirm,
+      }
     })()
   },
   head: {},
