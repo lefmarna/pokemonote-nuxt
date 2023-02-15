80f0288d5dddb1b743071112851ca51dd2df98d4
lefmarna <panume5ya@gmail.com>
2023-02-16 00:05:08 +0900

    fix: 広告のサイズ調整

--- components/organisms/Adsense.vue
+++ components/organisms/Adsense.vue
@@ -1,11 +1,9 @@
 <template>
   <ins
     class="adsbygoogle"
-    style="display: block"
+    style="display: inline-block; width: 232px; height: 232px"
     data-ad-client="ca-pub-3240586325286249"
-    data-ad-slot="4559543564"
-    data-ad-format="auto"
-    data-full-width-responsive="true"
+    data-ad-slot="2573612278"
   ></ins>
 </template>
 
