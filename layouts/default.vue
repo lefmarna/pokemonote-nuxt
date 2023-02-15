85a24760ef13310f70a04f6e32178eff04373e0a
lefmarna <panume5ya@gmail.com>
2023-02-16 00:14:03 +0900

    fix: スタイルの微修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -43,7 +43,7 @@
             </v-list-item-content>
           </v-list-item>
         </v-list>
-        <v-divider />
+        <v-divide class="mb-3" />
         <Adsense />
         <v-list-item>Others</v-list-item>
         <v-divider />
