c0502e402c32e218f7a6e3bb477bddaf626de55e
lefmarna <panume5ya@gmail.com>
2023-02-16 00:24:54 +0900

    fix: サイズ戻す

--- components/organisms/Adsense.vue
+++ components/organisms/Adsense.vue
@@ -1,7 +1,7 @@
 <template>
   <ins
     class="adsbygoogle"
-    style="display: inline-block; width: 232px; max-height: 300px"
+    style="display: inline-block; width: 232px; height: 232px"
     data-ad-client="ca-pub-3240586325286249"
     data-ad-slot="2573612278"
   ></ins>
